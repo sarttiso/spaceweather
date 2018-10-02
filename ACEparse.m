2935a22dadcfd4c46438b0472e5869477225b90c
Adrian Tasistro-Hart <adrian@tasistro.com>
2018-10-02 12:05:19 +0200

    full implementation of ACEparse.m

--- ACEparse.m
+++ ACEparse.m
@@ -15,6 +15,36 @@
 
 
 function [dat,t] = ACEparse(file,fields,varargin)
-    
+
+% parse inputs
+parser = inputParser;
+addRequired(parser,'file',@ischar)
+addRequired(parser,'fields',@iscell)
+
+parse(parser,file,fields,varargin{:})
+
+file = parser.Results.file;
+fields = parser.Results.fields;
+
+% get hdf file information
+finfo = hdfinfo(file);
+% now load data
+dat = hdfread(finfo.Vgroup(1).Vdata,...
+    'Fields',fields);
+% convert to numeric
+dat = cell2mat(dat)';
+
+% if user requested time, get it
+if nargout > 1
+    % load time data
+    curt = hdfread(finfo.Vgroup(1).Vdata,...
+        'Fields',{'year','day','hr','min','sec'});
+    % convert DOY to mm, dd
+    [mm,dd] = ddd2mmdd(double(curt{1}'),double(curt{2}'));
+    % create time vector
+    t = double([curt{1}',mm,dd,curt{3}',curt{4}',curt{5}']);
+    % convert to matlab time
+    t = datenum(t);
+end
 
 end
